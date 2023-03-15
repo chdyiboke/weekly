64f7e261526cda8934b9303f9652b57aad69ddbe
chdyiboke <caiyi2131@163.com>
2023-03-15 15:17:34 +0800

    添加vite

---
+++ packages/vite-project/README.md
@@ -0,0 +1,11 @@
+# Vue 3 + TypeScript + Vite
+
+This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+## QA
+
+### Q: [plugin:vite:vue] Single file component can contain only one <script setup
+
+A: 在上面有个 setup 没见到
+
+快快快
---
+++ packages/vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+defineProps<{ msg: string }>();
+const a: any = ref('ref-string');
+onMounted(() => {
+  a.value = '123...onMounted';
+});
+
+const count = ref(0);
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+    <p>
+      Edit
+      <code>HelloWorld.vue</code> to test HMR
+    </p>
+  </div>
+
+  <p>
+    {{ a }}
+    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+      >create-vue</a
+    >, the official Vue + Vite starter
+  </p>
+  <p>
+    Install
+    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    in your IDE for a better DX
+  </p>
+  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
